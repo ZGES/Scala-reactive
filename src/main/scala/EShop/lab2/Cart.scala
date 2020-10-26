2ba62290b047d1dfbbd4a5da175b7567991dce0a
Piotr Pasternak <piotrpassternak@gmail.com>
2020-10-26 23:29:34 +0100

    Cart implementation

--- src/main/scala/EShop/lab2/Cart.scala
+++ src/main/scala/EShop/lab2/Cart.scala
@@ -1,12 +1,25 @@
 package EShop.lab2
 
 case class Cart(items: Seq[Any]) {
-  def contains(item: Any): Boolean = ???
-  def addItem(item: Any): Cart     = ???
-  def removeItem(item: Any): Cart  = ???
-  def size: Int                    = ???
+  def contains(item: Any): Boolean = {
+     items.contains(item)
+  }
+
+  def addItem(item: Any): Cart ={
+     Cart(this.items :+ item)
+  }
+
+  def removeItem(item: Any): Cart  = {
+     Cart(this.items.diff(Seq(item)))
+  }
+
+  def size: Int = {
+    items.length
+  }
 }
 
 object Cart {
-  def empty: Cart = ???
+  def empty: Cart = {
+    Cart(Seq())
+  }
 }
